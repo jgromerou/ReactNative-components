4be37923f1d4eaf91344e18a588e6ab267eec0b4
jgromerou <jgromerouro@gmail.com>
2023-10-18 16:41:48 -0300

    se hizo la correcciones al probar el modal en el celular.

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -26,21 +26,29 @@ export const ModalScreen = () => {
             backgroundColor: 'rgba(0,0,0,0.3)',
             justifyContent: 'center',
             alignItems: 'center',
-            shadowOffset: {
-              width: 0,
-              height: 10,
-            },
-            shadowOpacity: 0.25,
-            elevation: 10,
-            borderRadius: 5,
           }}
         >
-          <View style={{ height: 200, width: 200, backgroundColor: 'white' }}>
+          {/* Contenido del Modal */}
+          <View
+            style={{
+              height: 200,
+              width: 200,
+              backgroundColor: 'white',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 5,
+            }}
+          >
             <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
-            <Text style={{ fontSize: 10, fontWeight: '300', marginBottom: 20 }}>
+            <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>
               Cuerpo del Modal
             </Text>
-            <HeaderTitle title="Modal" />
             <Button title="Cerrar" onPress={() => setIsVisible(false)} />
           </View>
         </View>
